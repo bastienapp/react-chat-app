76a66c0086c2ede520d1f1cd4ba4ceac349b383e
bastienwcs <bastien@wldcodeschool.fr>
2020-07-25 16:19:25 +0200

    fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 function Contact() {
+  const avatar = "https://randomuser.me/api/portraits/women/32.jpg";
+  const name = "Caroline Young";
+  const online = true;
 
-    const avatar = "https://randomuser.me/api/portraits/women/32.jpg";
-    const name = "Caroline Young";
-    const online = true;
-
-    return <div className="Contact">
-        <img className="avatar" src="https://randomuser.me/api/portraits/women/32.jpg"/>
-        <div>
-            <div className="name">Caroline Young</div>
-            <div className="status">
-                <span className={online ? 'status-online' : 'status-offline'}></span>
-                <span className="status-text">{online ? 'Online' : 'Offline'}</span>
-            </div>
+  return (
+    <div className="Contact">
+      <img className="avatar" src={avatar} alt="avatar" />
+      <div>
+        <div className="name">{name}</div>
+        <div className="status">
+          <span className={online ? "status-online" : "status-offline"} />
+          <span className="status-text">{online ? "Online" : "Offline"}</span>
         </div>
-    </div>;
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
