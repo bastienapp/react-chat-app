3bf4fca9fc37f3693ce140b4bfa152146c6a24a8
bastienwcs <bastien@wldcodeschool.fr>
2020-07-31 18:09:23 +0200

    add state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Contact.css";
+import { render } from "@testing-library/react";
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
 
-  return (
+  render() {
+    return (
       <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name} />
-      <div>
-        <div className="name">{props.name}</div>
-        <div className="status">
-          <span className={props.online ? "status-online" : "status-offline"} />
-          <span className="status-text">{props.online ? "Online" : "Offline"}</span>
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+          <div className="name">{this.props.name}</div>
+          <div
+            className="status"
+            onClick={(event) => {
+              this.setState({
+                online: !this.state.online,
+              });
+            }}
+          >
+            <span
+              className={this.state.online ? "status-online" : "status-offline"}
+            />
+            <span className="status-text">
+              {this.state.online ? "Online" : "Offline"}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Contact.propTypes = {
